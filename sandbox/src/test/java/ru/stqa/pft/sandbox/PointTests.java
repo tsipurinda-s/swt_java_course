030efe69cec071348928c77c11476eabba8e86dc
tsipurinda-s <tsipurinda.s@gmail.com>
2021-09-17 18:02:13 +0300

    Задание №3: Подключить тестовый фреймворк и сделать первые тесты

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testDistance1(){
+        Point p1 = new Point(3,1);
+        Point p2 = new Point(6,5);
+        double d = Point.distance(p1,p2);
+
+        Assert.assertEquals(d,5.0);
+    }
+
+    @Test
+    public void testDistance2(){
+        Point p1 = new Point(3,1);
+        Point p2 = new Point(9,9);
+        double d = Point.distance(p1,p2);
+        
+        Assert.assertEquals(d,10.0);
+    }
+}
