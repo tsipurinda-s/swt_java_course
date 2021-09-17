873758bb4ea62f244881fb64fbcab71488048c8a
tsipurinda-s <tsipurinda.s@gmail.com>
2021-09-17 16:21:53 +0300

    Задание №2. Вычисление расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,16 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-System.out.println("Hello, world!");
-	}
-	
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+
+        Point p1 = new Point(3,1);
+        Point p2 = new Point(6,5);
+
+        System.out.println("Заданы точки: p1(" + p1.x + "," + p1.y + "); p2(" + p2.x + "," + p2.y + ")");
+        System.out.println("Расстояние между двумя заданными точками: " + Point.distance(p1,p2));
+
+    }
+
 }
