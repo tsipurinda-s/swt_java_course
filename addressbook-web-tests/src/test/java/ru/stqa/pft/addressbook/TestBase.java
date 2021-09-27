8023c97b6637864a3dd8581c51640a2b20d5dfa2
tsipurinda-s <86415538+tsipurinda-s@users.noreply.github.com>
2021-09-27 23:11:25 +0300

    Создан GroupDeletionTests + вынесены методы в общий TestBase

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -0,0 +1,67 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    WebDriver wd;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String user, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).sendKeys(user);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    protected void goToGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    protected void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    protected void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.name());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.header());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.footer());
+    }
+
+    protected void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    protected void returnToGroupPage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        wd.quit();
+    }
+
+    protected void deleteSelectedGroups() {
+      wd.findElement(By.name("delete")).click();
+    }
+
+    protected void selectGroup() {
+      wd.findElement(By.name("selected[]")).click();
+    }
+}
